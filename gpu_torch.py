db4dc0d267ce896e2e5639132a75c84372ecdfc9
Evan <35814742+evanphilip@users.noreply.github.com>
2025-02-18 15:19:26 +0100

    Create gpu_torch.py

---
+++ gpu_torch.py
@@ -0,0 +1,22 @@
+"""
+Checks if PyTorch is able to leverage the GPU by running a minimal benchmark.
+"""
+import torch
+import torch.utils.benchmark as benchmark
+
+
+def job(gpu):
+    device = "cuda" if gpu else "cpu"
+    x = torch.randn(10000, 1024, device=device)
+    return benchmark.Timer(
+        stmt="x.mul(x).sum(-1)",
+        globals={"x": x},
+        label="Benchmark",
+        description=device,
+    ).timeit(100)
+
+
+print(__doc__)
+print(f"GPU Device Name : {torch.cuda.get_device_name()}\n")
+results = [job(gpu=False), job(gpu=True)]
+benchmark.Compare(results).print()
